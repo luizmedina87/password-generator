8ab525b5014e16052818180964a15628859da0bd
Luiz Medina <luizmedina87@gmail.com>
2022-03-10 12:27:52 -0500

    created prompts to get chars and forced to choose one

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-var getChars = function() {
-  // prompt user for which characters they want to use (select at least one)
+var getChars = function(chars) {
+
+  // create variables to determine which characters to use
+  var includeLower, includeUpper, includeNumeric, includeSpecial;
+  includeLower = includeUpper = includeNumeric = includeSpecial = true;
+
+  // variable to check if the user selected none of the options
+  var selectedOption = false;
+
+  // enter a loop that forces the user to choose at least one option
+  while (!selectedOption) {
+    debugger;
+    // get user input
+    window.alert(
+      "Select the characters to use in your password.\n"
+      + "You must SELECT AT LEAST ONE group of characters."
+    )
+    includeLower = window.confirm(
+      "Include lowercase characters?\n"
+      + chars.lower
+    );
+    includeUpper = window.confirm(
+      "Include upercase characters?\n"
+      + chars.upper
+    );
+    includeNumeric = window.confirm(
+      "Include numeric characters?\n"
+      + chars.numeric
+    );
+    includeSpecial = window.confirm(
+      "Include special characters?\n"
+      + chars.special
+    )
+
+    // check if user provided at least one option
+    selectedOption = (
+      includeLower || includeUpper || includeNumeric || includeSpecial
+    );
+  }
 }
 
 var getSize = function() {
@@ -16,7 +53,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var validChars = getChars();
+  var validChars = getChars(passwordChars);
   var passwordSize = getSize();
   var password = generatePassword(validChars, passwordSize);
   var passwordText = document.querySelector("#password");
@@ -29,8 +66,8 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 passwordChars = {
-  lower: [..."abcdefghijklmnopqrstuvwyxz"],
-  upper: [..."ABCDEFGHIJKLMNOPQRSTUVWYXZ"],
-  special: [..."!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"],
-  numeric: [..."1234567890"]
+  lower: ["abcdefghijklmnopqrstuvwyxz"],
+  upper: ["ABCDEFGHIJKLMNOPQRSTUVWYXZ"],
+  numeric: ["1234567890"],
+  special: ["!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"]
 }
